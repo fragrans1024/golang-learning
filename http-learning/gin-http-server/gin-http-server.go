78915ef1a8dc4e85b0e32f06588d32d60fe1b6dd
fragrans1024 <today_wsl@163.com>
2025-10-05 09:09:57 +0800

    gin分组路由

--- http-learning/gin-http-server/gin-http-server.go
+++ http-learning/gin-http-server/gin-http-server.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin_default()
+	r := gin_group()
 
 	r.Run("127.0.0.1:9001")
 }
@@ -16,4 +16,33 @@ func gin_default() *gin.Engine {
 	})
 
 	return router
+}
+
+// 分组路由
+func gin_group() *gin.Engine {
+	router := gin.New()
+
+	router_v1 := router.Group("v1")
+	router_v1.GET("/ping", ping_v1)
+
+	router_v2 := router.Group("v2")
+	router_v2.GET("/ping", ping_v2)
+
+	return router
+}
+
+func ping_v1(c *gin.Context) {
+	resp := gin.H{
+		"message": "pong",
+		"version": "v1",
+	}
+	c.JSON(200, resp)
+}
+
+func ping_v2(c *gin.Context) {
+	resp := gin.H{
+		"message": "pong",
+		"version": "v2",
+	}
+	c.JSON(200, resp)
 }
