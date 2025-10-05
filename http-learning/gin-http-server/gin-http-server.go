e583a6f8beb9cbdce8b846706a3bfc4616105356
fragrans1024 <today_wsl@163.com>
2025-10-05 09:06:26 +0800

    gin default，简单封装

--- http-learning/gin-http-server/gin-http-server.go
+++ http-learning/gin-http-server/gin-http-server.go
@@ -3,9 +3,17 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r := gin_default()
+
+	r.Run("127.0.0.1:9001")
+}
+
+func gin_default() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
-	r.Run("127.0.0.1:9001")
+
+	return router
 }
